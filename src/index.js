39812d710e34b5dc5a750178c6c3d8ea882ac2d2
Marvin Alegre <marvinalegredev@gmail.com>
2026-08-18 20:23:37 +0800

    add forge api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-export default "hello from mathforge";
+const generators = {
+  "addition-single-digit-no-carry": () => {
+    const a = randomInteger(1, 8);
+    const b = randomInteger(1, 9 - a);
+
+    return {
+      data: { a, b },
+      answer: a + b,
+    };
+  },
+};
+
+export function forge(id) {
+  if (!generators[id]) {
+    throw new Error(`Unknown problem: ${id}`);
+  }
+
+  return generators[id]();
+}
+
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
